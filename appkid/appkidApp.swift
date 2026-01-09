6bfe9f7b422f54ef7cfc6c771aae0d8215599fa9
kovacsalex99 <you@example.com>
2026-01-09 18:14:39 +0200

    Initial Commit

---
+++ appkid/appkidApp.swift
@@ -0,0 +1,17 @@
+//
+//  appkidApp.swift
+//  appkid
+//
+//  Created by Alex Kovacs on 09.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct appkidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ appkidTests/appkidTests.swift
@@ -0,0 +1,17 @@
+//
+//  appkidTests.swift
+//  appkidTests
+//
+//  Created by Alex Kovacs on 09.01.2026.
+//
+
+import Testing
+@testable import appkid
+
+struct appkidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
